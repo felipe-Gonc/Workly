0330d72e1219787a1fc2334d8d5787323e976c61
Matheus Felipe <mf6232103@gmail.com>
2026-01-13 21:23:22 -0300

    Initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import authRoutes from "./routes/auth.route.js"
+
+const app = express();
+const PORT = process.env.PORT || 5001
+
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT, () => {
+  console.log("Server rodando na porta:", PORT);
+});
